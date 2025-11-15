a66b6e95a8d5b3799a5998ad9b3874ab9a27c713
Davi Francisco <franciscodavi327@gmail.com>
2025-11-15 17:52:04 -0300

    Usar contexto para autenticação

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,9 +2,10 @@ import Header from './components/Header/Header';
 import Home from './pages/Home/Home';
 import LandingPage from './pages/LandingPage/LandingPage';
 import Footer from './components/Footer/Footer';
+import { useAuthenticated } from './context/AuthContext';
 
 export default function App() {
-    const isAuthenticated = false;
+    const { isAuthenticated } = useAuthenticated();
 
     return (
         <div className='wrapper'>
