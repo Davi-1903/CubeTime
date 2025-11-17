bb9934876d11f2e8f6bd5f7cedca84d2afbcab90
Davi Francisco <franciscodavi327@gmail.com>
2025-11-17 18:57:42 -0300

    Fazer o cronômetro funcionar

--- client/src/pages/Stopwatcher/Stopwatcher.jsx
+++ client/src/pages/Stopwatcher/Stopwatcher.jsx
@@ -1,10 +1,13 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { IconPlayerPause, IconPlayerPlay, IconRefresh } from '@tabler/icons-react';
+import PrivateRoute from '../../components/PrivateRoute/PrivateRoute';
 import './Stopwatcher.css';
 
 export default function Stopwatcher() {
     const [isStart, setStart] = useState(false);
     const [time, setTime] = useState(0); // Tempo em milissegundos
+    const startTime = useRef(null);
+    const pausedTime = useRef(0);
 
     function formatTime(time) {
         const hours = String(Math.floor((time / 3600000) % 60)).padStart(2, '0');
@@ -15,32 +18,60 @@ export default function Stopwatcher() {
     }
 
     function handleRefresh() {
-        if (!isStart) setTime(0);
+        if (!isStart) {
+            setTime(0);
+            pausedTime.current = 0;
+            startTime.current = null;
+        }
     }
 
     function handleStart() {
-        setStart(!isStart);
+        setStart(true);
     }
 
+    function handleStop() {
+        setStart(false);
+        pausedTime.current = time;
+    }
+
+    useEffect(() => {
+        let animationId;
+
+        if (isStart) {
+            startTime.current = performance.now() - pausedTime.current;
+
+            const update = currentTime => {
+                setTime(currentTime - startTime.current);
+                animationId = requestAnimationFrame(update);
+            };
+
+            animationId = requestAnimationFrame(update);
+        }
+
+        return () => cancelAnimationFrame(animationId);
+    }, [isStart]);
+
     return (
-        <div className='stopwatcher-container'>
-            <article className='stopwatcher-content'>
-                <h1 id='stopwatcher'>{formatTime(time)}</h1>
-                <div className='functions-container'>
-                    {isStart ? (
-                        <button id='stop' onClick={handleStart}>
-                            <IconPlayerPause size={36} />
+        <PrivateRoute>
+            <div className='stopwatcher-container'>
+                <article className='stopwatcher-content'>
+                    <h1 id='stopwatcher'>{formatTime(time)}</h1>
+                    <div className='functions-container'>
+                        {isStart ? (
+                            <button id='stop' onClick={handleStop}>
+                                <IconPlayerPause size={36} />
+                            </button>
+                        ) : (
+                            <button id='start' onClick={handleStart}>
+                                <IconPlayerPlay size={36} />
+                            </button>
+                        )}
+                        <button id='refresh' onClick={handleRefresh} disabled={isStart}>
+                            <IconRefresh size={36} />
                         </button>
-                    ) : (
-                        <button id='start' onClick={handleStart}>
-                            <IconPlayerPlay size={36} />
-                        </button>
-                    )}
-                    <button id='refresh' onClick={handleRefresh} disabled={isStart}>
-                        <IconRefresh size={36} />
-                    </button>
-                </div>
-            </article>
-        </div>
+                    </div>
+                </article>
+            </div>
+        </PrivateRoute>
     );
 }
