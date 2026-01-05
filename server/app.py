118254c3ff3660e2c63be2fc1f013386b53cb25f
Davi Francisco <franciscodavi327@gmail.com>
2026-01-05 14:25:43 -0300

    build: otimizar imagens

--- server/app.py
+++ server/app.py
@@ -1,4 +1,3 @@
-import os
 from flask import Flask
 from controllers.auth import auth_bp
 from controllers.user import user_bp
@@ -6,23 +5,9 @@ from controllers.time import time_bp
 from config import config_app
 
 
-app = Flask(__name__, static_folder='../client/dist')
+app = Flask(__name__)
 config_app(app)
 
 app.register_blueprint(auth_bp)
 app.register_blueprint(user_bp)
 app.register_blueprint(time_bp)
-
-# Rota "Catch-All"
-@app.route('/')
-@app.route('/<path:path>')
-def serve_react(path: str = ''):
-    if app.static_folder is None:
-        raise RuntimeError('STATIC_FOLDER não foi definido')
-    if path and not path.startswith('/api') and os.path.exists(os.path.join(app.static_folder, path)):
-        return app.send_static_file(path)
-    return app.send_static_file('index.html')
-
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
